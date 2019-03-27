193e63c4f17ee6a5572f4013aab8c58800f8ef31
grayGit <gray@enclave.vn>
2019-03-27 15:41:03 +0700

    Added login form and input validation

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.generateForm();
+  }
+  // create form by using form builder
+  generateForm(): void {
+   this.loginForm = this.formBuilder.group({
+     username: ['', Validators.required],
+     password: ['', [Validators.required, Validators.minLength(8)]]
+   });
+  }
+
+}
