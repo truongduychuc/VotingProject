d3948bf5ae0e64b1dd3d3234dd17463405d6f261
Truong Duy Chuc <truongduychuc138@gmail.com>
2019-03-31 22:07:22 +0700

    Changed login method

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import {AuthenticationService} from "../services/authentication.service";
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -8,17 +8,35 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class LoginFormComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthenticationService) { }
 
   ngOnInit() {
     this.generateForm();
+    this.authService.logout();
   }
   // create form by using form builder
-  generateForm(): void {
+  private generateForm(): void {
    this.loginForm = this.formBuilder.group({
      username: ['', Validators.required],
      password: ['', [Validators.required, Validators.minLength(8)]]
    });
   }
+
+  loginWithUserAndPass() {
+    if(this.loginForm.invalid) {
+      return;
+    }
+    const username = this.loginForm.controls['username'].value;
+    const password = this.loginForm.controls['password'].value;
+    this.authService.login(username, password).subscribe(res => {
+    console.log(res);
+    }, error1 => console.log(error1));
+  }
+
+  // get all controls of loginForm, can use this function with command like getControl.controlName;
+  get getControl() {
+    return this.loginForm.controls;
+  }
+
 
 }
