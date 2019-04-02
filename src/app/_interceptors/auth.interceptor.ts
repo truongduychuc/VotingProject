c76b68bafe90ac70285d02c2d24e4093916921fd
grayGit <gray@enclave.vn>
2019-04-02 17:05:31 +0700

    Added interceptors

---
+++ src/app/_interceptors/auth.interceptor.ts
@@ -0,0 +1,11 @@
+/*
+import {Injectable} from "@angular/core";
+import {HttpInterceptor} from "@angular/common/http";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{  // attach token for every request to backend
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+  }
+}
+*/
