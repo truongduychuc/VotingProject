f0f140c1aae1f4de044b63acece62d3b433a508c
grayGit <gray@enclave.vn>
2019-04-03 16:50:24 +0700

    Updated canActivate method

--- src/app/guards/authentication.guard.ts
+++ src/app/guards/authentication.guard.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthenticationService} from "../services/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
+  constructor(private authService: AuthenticationService, private router: Router) {
+  }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if(this.authService.isLoggedOut()) {
+      this.router.navigate(['/login']);
+      return false;
+    }
     return true;
   }
 }
