5e01f8b3d1f12872a6d90e7f878fd34641bcaad0
grayGit <gray@enclave.vn>
2019-04-03 16:48:25 +0700

    Updated intercept method

--- src/app/_interceptors/auth.interceptor.ts
+++ src/app/_interceptors/auth.interceptor.ts
@@ -1,11 +1,20 @@
-/*
+
 import {Injectable} from "@angular/core";
-import {HttpInterceptor} from "@angular/common/http";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor{  // attach token for every request to backend
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+    const loginToken = localStorage.getItem('token');
+    if(loginToken) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', loginToken)
+      });
+      return next.handle(cloned);
+    }
+    else {
+      return next.handle(req);
+    }
   }
 }
-*/
