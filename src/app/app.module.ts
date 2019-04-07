9c155ac255bbd574be4df7876d1147f4970d6357
Truong Duy Chuc <truongduychuc138@gmail.com>
2019-04-07 07:37:14 +0700

    Updated Employeelist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsComponent } from './forms/forms.component';
-import { ButtonsComponent } from './buttons/buttons.component';
-import { TablesComponent } from './tables/tables.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { AlertsComponent } from './alerts/alerts.component';
-import { AccordionsComponent } from './accordions/accordions.component';
-import { BadgesComponent } from './badges/badges.component';
-import { ProgressbarComponent } from './progressbar/progressbar.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { DropdownComponent } from './dropdown/dropdown.component';
-import { TooltipsComponent } from './tooltips/tooltips.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { CreateUserFormComponent } from './create-user-form/create-user-form.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {FooterComponent} from './footer/footer.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CreateUserFormComponent} from './create-user-form/create-user-form.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { LoginFormComponent } from './login-form/login-form.component';
+import {LoginFormComponent} from './login-form/login-form.component';
 import {AuthInterceptor} from "./_interceptors/auth.interceptor";
 import {ErrorInterceptor} from "./_interceptors/error.interceptor";
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-
+import {EmployeeListComponent} from './employee-list/employee-list.component';
+import {InputsModule, TableModule, IconsModule} from "angular-bootstrap-md";
 @NgModule({
   declarations: [
     AppComponent,
@@ -40,24 +23,9 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
     SidebarComponent,
     FooterComponent,
     DashboardComponent,
-    FormsComponent,
-    ButtonsComponent,
-    TablesComponent,
-    TypographyComponent,
-    IconsComponent,
-    AlertsComponent,
-    AccordionsComponent,
-    BadgesComponent,
-    ProgressbarComponent,
-    BreadcrumbsComponent,
-    PaginationComponent,
-    DropdownComponent,
-    TooltipsComponent,
-    CarouselComponent,
-    TabsComponent,
     CreateUserFormComponent,
     LoginFormComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
   ],
   imports: [
     BrowserModule,
@@ -66,7 +34,10 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
     FormsModule,
     NgbModule.forRoot(),
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    TableModule,
+    IconsModule,
+    InputsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AccountService} from "../_services/account.service";
-
+import {MdbTableService} from "angular-bootstrap-md";
 
 @Component({
   selector: 'app-employee-list',
@@ -8,15 +8,43 @@ import {AccountService} from "../_services/account.service";
   styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent implements OnInit {
-
+  CURRENT_SORT_ICON = 'sort';
+  searchKeyWord : string = '';
+  previous: string;
   itemsPerPage = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
   item: number;
-  constructor(private accountService: AccountService) { }
-  listOfAllEmployee = [
+  constructor(private accountService: AccountService, private mdbTableService: MdbTableService) { }
+  listOfEmployees = [
     {username: 'gray' ,password: '12345678', first_name: 'Chuc', last_name: 'Truong', english_name: 'Gray', email: 'gray@enclave.vn', position: 'developer', team: 'Troy'},
     {username: 'roger' ,password: '12345678', first_name: 'Dinh', last_name: 'Le', english_name: 'Roger', email: 'roger@enclave.vn', position: 'developer', team: 'Troy'}
   ];
   ngOnInit() {
+    this.mdbTableService.setDataSource(this.listOfEmployees);
+    this.listOfEmployees = this.mdbTableService.getDataSource();
+    this.previous = this.mdbTableService.getDataSource();
+  }
+  onChangeDirection() {
+    if(this.CURRENT_SORT_ICON === 'sort') {
+      this.CURRENT_SORT_ICON = 'sort-up';
+    }
+    if(this.CURRENT_SORT_ICON === 'sort-up') {
+      this.CURRENT_SORT_ICON = 'sort-down';
+    }
+    if(this.CURRENT_SORT_ICON === 'sort-down') {
+      this.CURRENT_SORT_ICON = 'sort-up';
+    }
+  }
+  searchRecords() {
+    const prev = this.mdbTableService.getDataSource();
+    if(!this.searchKeyWord) {
+      this.mdbTableService.setDataSource(this.previous);
+      this.listOfEmployees = this.mdbTableService.getDataSource();
+    }
+
+    if(this.searchKeyWord) {
+      this.listOfEmployees = this.mdbTableService.searchLocalDataBy(this.searchKeyWord);
+      this.mdbTableService.setDataSource(prev);
+    }
   }
 
 }
