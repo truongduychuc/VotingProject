9d62de1b0a2d6671c503b0a8d2c563f78ada6f4e
Truong Duy Chuc <truongduychuc138@gmail.com>
2019-04-07 23:01:05 +0700

    Added employeelist component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,28 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {AccountService} from "../_services/account.service";
-import {MdbTableService} from "angular-bootstrap-md";
+import {MdbTablePaginationComponent, MdbTableService} from "angular-bootstrap-md";
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.scss']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, AfterViewInit {
+  @ViewChild(MdbTablePaginationComponent) mdbTablePagination: MdbTablePaginationComponent;
   CURRENT_SORT_ICON = 'sort';
   searchKeyWord : string = '';
   previous: string;
   itemsPerPage = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-  item: number;
-  constructor(private accountService: AccountService, private mdbTableService: MdbTableService) { }
   listOfEmployees = [
     {username: 'gray' ,password: '12345678', first_name: 'Chuc', last_name: 'Truong', english_name: 'Gray', email: 'gray@enclave.vn', position: 'developer', team: 'Troy'},
     {username: 'roger' ,password: '12345678', first_name: 'Dinh', last_name: 'Le', english_name: 'Roger', email: 'roger@enclave.vn', position: 'developer', team: 'Troy'}
   ];
+
+  firstItemIndex;
+  lastItemIndex;
+  constructor(private accountService: AccountService, private mdbTableService: MdbTableService, private cdRef:ChangeDetectorRef) { }
+  @HostListener('input') oninput() {
+    this.searchRecords();
+  }
   ngOnInit() {
     this.mdbTableService.setDataSource(this.listOfEmployees);
     this.listOfEmployees = this.mdbTableService.getDataSource();
     this.previous = this.mdbTableService.getDataSource();
   }
+  ngAfterViewInit(): void {
+  }
+
   onChangeDirection() {
     if(this.CURRENT_SORT_ICON === 'sort') {
       this.CURRENT_SORT_ICON = 'sort-up';
