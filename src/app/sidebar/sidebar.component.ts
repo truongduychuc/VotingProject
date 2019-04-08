db15817a19e7c3b11709c74296bad38ed437c2d2
Truong Duy Chuc <truongduychuc138@gmail.com>
2019-04-08 20:53:44 +0700

    change pipe() to subscribe()

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../_services/authentication.service";
+import {AccountService} from "../_services/account.service";
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,12 +11,21 @@ import {AuthenticationService} from "../_services/authentication.service";
 })
 export class SidebarComponent implements OnInit {
   public samplePagesCollapsed = true;
-
-  constructor(private router: Router, private authService: AuthenticationService) {
+  currentUser : any;
+  constructor(private router: Router, private authService: AuthenticationService, private accountService: AccountService) {
 
   }
 
   ngOnInit() {
+    console.log('Inited!');
+    this.accountService.getProfile().subscribe(
+      user => {
+        this.currentUser = user;
+        console.log(user);
+      }, error1 => {
+        console.log(error1);
+      }
+    );
   }
   logout() {
     this.authService.logout();
