5025292dc0c2dd07ecbb9a4e26f632e693b22f96
grayGit <gray@enclave.vn>
2019-04-08 12:04:27 +0700

    Added alert commnand for successful message

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -1,15 +1,28 @@
 import {Injectable} from "@angular/core";
-import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent, HttpErrorResponse} from "@angular/common/http";
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpEvent,
+  HttpErrorResponse,
+  HttpResponse
+} from "@angular/common/http";
 import {AuthenticationService} from "../_services/authentication.service";
 import {Observable, throwError} from "rxjs";
-import {catchError} from "rxjs/operators";
+import {catchError, tap} from "rxjs/operators";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
   constructor(private authService: AuthenticationService) {
   }
  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(catchError((err:HttpErrorResponse) => {
+    return next.handle(req).pipe(tap(evt => {
+      if (evt instanceof HttpResponse) {
+        if (evt.body && evt.body.success) {
+          console.log(evt.body.success.message);
+        }
+      }
+    }),catchError((err:HttpErrorResponse) => {
       if(err.status === 401) {
         this.authService.logout();
         location.reload(true);
