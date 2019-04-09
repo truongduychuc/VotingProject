159c6447a1c856f437dae7002ceaddd3db08f66a
grayGit <gray@enclave.vn>
2019-04-09 11:14:01 +0700

    Changed profile displaying

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {AuthenticationService} from "../_services/authentication.service";
 import {AccountService} from "../_services/account.service";
 import {map} from 'rxjs/operators';
+import {User} from "../_models/user";
 
 @Component({
   selector: 'app-sidebar',
@@ -11,25 +12,27 @@ import {map} from 'rxjs/operators';
 })
 export class SidebarComponent implements OnInit {
   public samplePagesCollapsed = true;
-  currentUser : any;
+  currentUser : User;
   constructor(private router: Router, private authService: AuthenticationService, private accountService: AccountService) {
-
+    this.initProfileInfo();
   }
 
   ngOnInit() {
     console.log('Inited!');
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+  initProfileInfo() {
     this.accountService.getProfile().subscribe(
-      user => {
+      (user: User) => {
         this.currentUser = user;
         console.log(user);
       }, error1 => {
         console.log(error1);
       }
     );
-  }
-  logout() {
-    this.authService.logout();
-    this.router.navigate(['login']);
   }
 
 }
