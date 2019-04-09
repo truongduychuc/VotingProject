990f0aa12a9fa55652a4077cf496e959d3c7bcdb
grayGit <gray@enclave.vn>
2019-04-09 17:04:43 +0700

    Added data-tracking as an alternative for old dashboard component

---
+++ src/app/dashboard/data-tracking/data-tracking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-tracking',
+  templateUrl: './data-tracking.component.html',
+  styleUrls: ['./data-tracking.component.scss']
+})
+export class DataTrackingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
