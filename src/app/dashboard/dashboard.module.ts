632c0ff63134ad527cb53482e33987142445e68f
grayGit <gray@enclave.vn>
2019-04-11 11:57:17 +0700

    Employee's list pagination

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule } from '@angular/core';
+import {CommonModule } from '@angular/common';
 import {ChangePasswordFormComponent} from "./change-password-form/change-password-form.component";
 import {CreateUserFormComponent} from "./create-user-form/create-user-form.component";
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
@@ -7,11 +7,12 @@ import {FooterComponent} from "./footer/footer.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {SidebarComponent} from "./sidebar/sidebar.component";
 import {IconsModule, InputsModule, TableModule} from "angular-bootstrap-md";
-import { DataTrackingComponent } from './data-tracking/data-tracking.component';
+import {DataTrackingComponent } from './data-tracking/data-tracking.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DashboardRoutingModule} from "./dashboard-routing.module";
 import {RouterModule} from "@angular/router";
 import {DashboardComponent} from "./dashboard.component";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {DashboardComponent} from "./dashboard.component";
     DashboardRoutingModule,
     TableModule,
     IconsModule,
-    InputsModule
+    InputsModule,
+    NgbModule
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/employee-list/employee-list.component.ts
+++ src/app/dashboard/employee-list/employee-list.component.ts
@@ -8,52 +8,19 @@ import {MdbTablePaginationComponent, MdbTableService} from "angular-bootstrap-md
   styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent implements OnInit, AfterViewInit {
-  @ViewChild(MdbTablePaginationComponent) mdbTablePagination: MdbTablePaginationComponent;
-  CURRENT_SORT_ICON = 'sort';
-  searchKeyWord : string = '';
-  previous: string;
-  itemsPerPage = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  page = 1;
+  pageSize = 5;
+  itemsPerPageArr = [5,10,15,20,25];
   listOfEmployees = [
     {username: 'gray' ,password: '12345678', first_name: 'Chuc', last_name: 'Truong', english_name: 'Gray', email: 'gray@enclave.vn', position: 'developer', team: 'Troy'},
     {username: 'roger' ,password: '12345678', first_name: 'Dinh', last_name: 'Le', english_name: 'Roger', email: 'roger@enclave.vn', position: 'developer', team: 'Troy'}
   ];
-
-  firstItemIndex;
-  lastItemIndex;
-  constructor(private accountService: AccountService, private mdbTableService: MdbTableService, private cdRef:ChangeDetectorRef) { }
-  @HostListener('input') oninput() {
-    this.searchRecords();
-  }
+  constructor(private accountService: AccountService) { }
   ngOnInit() {
-    this.mdbTableService.setDataSource(this.listOfEmployees);
-    this.listOfEmployees = this.mdbTableService.getDataSource();
-    this.previous = this.mdbTableService.getDataSource();
+
   }
   ngAfterViewInit(): void {
   }
 
-  onChangeDirection() {
-    if(this.CURRENT_SORT_ICON === 'sort') {
-      this.CURRENT_SORT_ICON = 'sort-up';
-    }
-    if(this.CURRENT_SORT_ICON === 'sort-up') {
-      this.CURRENT_SORT_ICON = 'sort-down';
-    }
-    if(this.CURRENT_SORT_ICON === 'sort-down') {
-      this.CURRENT_SORT_ICON = 'sort-up';
-    }
-  }
-  searchRecords() {
-    const prev = this.mdbTableService.getDataSource();
-    if(!this.searchKeyWord) {
-      this.mdbTableService.setDataSource(this.previous);
-      this.listOfEmployees = this.mdbTableService.getDataSource();
-    }
-
-    if(this.searchKeyWord) {
-      this.listOfEmployees = this.mdbTableService.searchLocalDataBy(this.searchKeyWord);
-      this.mdbTableService.setDataSource(prev);
-    }
-  }
 
 }
