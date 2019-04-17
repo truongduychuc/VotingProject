e02575358ddfb9cacb0a2d03fe8a192fd8626134
grayGit <gray@enclave.vn>
2019-04-17 17:04:23 +0700

    Updated list of employee's component

--- src/app/dashboard/personal-information/personal-information.component.ts
+++ src/app/dashboard/personal-information/personal-information.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../_models/user";
+import {AuthenticationService} from "../../_services/authentication.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-personal-information',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-information.component.scss']
 })
 export class PersonalInformationComponent implements OnInit {
+  editable: boolean;
+  currentUserProfile: User;
 
-  constructor() { }
+  address: string;
+  phone: string;
+  other: string;
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+    this.editable = false;
+    this.getUserInfo();
   }
-
+  changeEditable() {
+    this.editable = !this.editable;
+  }
+  getUserInfo() {
+    this.authService.getPersonalProfile().subscribe((userProfileRes:User) => {
+      this.currentUserProfile = userProfileRes;
+    }, (err: HttpErrorResponse) => {
+      console.log(err + ' status: ' +err.status);
+    })
+  }
 }
