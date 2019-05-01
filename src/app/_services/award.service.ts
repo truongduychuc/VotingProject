952337ca41df70da62341e98e6645952175a46de
Truong Duy Chuc <truongduychuc138@gmail.com>
2019-05-01 23:44:08 +0700

    Added award service

---
+++ src/app/_services/award.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Award} from "../_models/award";
+import {PastWinner} from "../_models/past-winner";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AwardService {
+  serverURL = 'http://localhost:4000/awards/';
+  constructor(private httpClient: HttpClient) {
+
+  }
+  getAwardList(): Observable<Award[]> {
+    return this.httpClient.get<Award[]>(this.serverURL + 'list');
+  }
+  updateAward(id: number, newInfo: Object) {
+    return this.httpClient.put(this.serverURL + `update/${id}`, newInfo);
+  }
+  uploadLogo(id: number, file: File) {
+    return this.httpClient.post(this.serverURL + `upload_logo/${id}`, {logo: file});
+  }
+
+  getAwardDetail(id: number): Observable<Award> {
+    return this.httpClient.get<Award>(this.serverURL + `${id}`);
+  }
+  getPastWinner(id: number, params?: HttpParams): Observable<PastWinner> {
+    return this.httpClient.get<PastWinner>(this.serverURL + `past_winner/${id}`, {params: params});
+  }
+  getRankingBreakDown(id: number, params?: HttpParams) {
+    return this.httpClient.get(this.serverURL + `breakdown/${id}`, {params: params});
+  }
+}
