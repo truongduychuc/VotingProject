505aba0716e6fbedbd6f5fb64dbad99beccafb1d
Truong Duy Chuc <truongduychuc138@gmail.com>
2019-05-06 00:14:40 +0700

    Updated at onSubmit and generateForm

--- src/app/home/award-list/add-award-modal/add-award-modal.component.ts
+++ src/app/home/award-list/add-award-modal/add-award-modal.component.ts
@@ -1,8 +1,11 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbActiveModal, NgbCalendar, NgbDateNativeAdapter, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 import {TeamService} from '../../../_services/team.service';
 import {AwardService} from '../../../_services/award.service';
-import {FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {map} from "rxjs/operators";
+import {Team} from "../../../_models/team";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-add-award',
@@ -13,35 +16,97 @@ import {FormGroup} from '@angular/forms';
 export class AddAwardModalComponent implements OnInit {
   years: number[] = [];
   types: any[];
-  nominees: any[];
-  showOtherNameInput = false;
-nominessMock = [
-  {id: 1, name: 'Roger', team: 'Bootcamp'},
-  {id: 2, name: 'Roger', team: 'Manager'},
-  {id: 3, name: 'Roger', team: 'Cadet'}
-];
-selected: any[];
+  nomineesList: any[];
+  listTeams: Team[]; // for finding and patching team_name to nominee object
   addAward: FormGroup;
-  constructor(public activeModal: NgbActiveModal, private teamService: TeamService, private awardService: AwardService) { }
+
+  dateStartMin: NgbDateStruct;
+  dateStartMax: NgbDateStruct;
+  dateEndMin: NgbDateStruct;
+  dateEndMax: NgbDateStruct;
+
+  constructor(public activeModal: NgbActiveModal, private teamService: TeamService, private awardService: AwardService,
+              private formBuilder: FormBuilder, private calendar: NgbCalendar, private dateNative: NgbDateNativeAdapter) { }
 
   ngOnInit() {
     this.getListNominees();
     this.getAwardTypes();
+    this.setDateInitially();
+    this.getListTeams();
     this.setYearsArray();
+    this.generateForm();
+  }
+  generateForm() {
+    this.addAward = this.formBuilder.group({
+      type: '',
+      name: '',
+      year: ['', Validators.required],
+      id_nominee: ['', Validators.required],
+      id_role_voter: ['Who?', Validators.required],
+      date_start: [this.dateStartMin, Validators.required], // every award can only have date_start chosen from today
+      date_end: ['', Validators.required],
+      prize: ['', Validators.required],
+      item: '',
+      description: ''
+    });
+  }
+  setDateInitially() {
+    this.dateStartMin = this.calendar.getToday();
+    this.dateStartMax = <NgbDateStruct> {
+      year: this.dateStartMin.year,
+      month: 12,
+      day: 31
+    };
+    this.dateEndMin = this.dateStartMin;
+    this.dateEndMax = <NgbDateStruct>{
+      year: this.dateStartMin.year+1,
+      month: this.dateStartMin.month,
+      day: this.dateStartMin.day
+    };
+  }
+  changeDateEndLimit() {
+    let currentDateStart = this.addAward.controls['date_start'].value;
+    console.log(new Date());
+    console.log(currentDateStart);
+    this.dateEndMin = currentDateStart;
+    this.dateEndMax = <NgbDateStruct>{
+      year: currentDateStart.year+1,
+      month: currentDateStart.month,
+      day: currentDateStart.day
+    };
   }
   setYearsArray() {
     const currentYear = new Date().getFullYear();
     this.years.push(currentYear);
     this.years.push(currentYear - 1);
-    console.log(this.years);
+    // console.log(this.years);
   }
   getListNominees() {
     this.teamService.getListForNominating().subscribe( successRes => {
-      this.nominees  = successRes.data;
-      console.log(this.nominees);
+      this.nomineesList = successRes.data;
+      // console.log(this.nomineesList);
     }, error => {
       console.log(error);
     });
+  }
+  getListTeams() {
+    this.teamService.getAllTeams().subscribe( teams => {
+      this.listTeams = teams;
+      console.log(this.nomineesList);
+      if(!this.nomineesList) {
+        console.log('Nominee list is undefined!');
+      } else {
+        this.nomineesList.forEach(value => {
+          let team = this.listTeams.find(team => team.id === value.id_team);
+          if(!team) {
+            console.log('Error when finding equivalent team!');
+          } else {
+            value.team_name = team.name;
+          }
+        });
+        console.log(this.nomineesList);
+      }
+    }, error1 => console.log(error1));
   }
   getAwardTypes() {
     this.awardService.getAwardTypes().subscribe( successRes => {
@@ -55,13 +120,32 @@ selected: any[];
       console.log(error);
     });
   }
-  onAwardNameSelectChanged() {
-    const nameSelected = (<HTMLSelectElement>document.getElementById('awardName')).value;
-    console.log(nameSelected);
-    if (nameSelected !== 'other') {
-      this.showOtherNameInput = false;
+  // onSubmit
+  createNewAward() {
+    if(this.addAward.invalid) {
+      console.log('Invalid!');
+      return;
     } else {
-      this.showOtherNameInput = true;
+      this.formControl['date_start'].setValue(this.dateNative.toModel(this.formControl['date_start'].value));
+      this.formControl['date_end'].setValue(this.dateNative.toModel(this.formControl['date_end'].value));
+      console.log(this.addAward.value);
+      this.awardService.createNewAward(this.addAward.value).subscribe( success => {
+        this.activeModal.close('Success!');
+      }, err => {
+        console.log(err);
+      });
     }
   }
+  get formControl() {
+    return this.addAward.controls;
+  }
+  // onAwardNameSelectChanged() {
+  //   const nameSelected = (<HTMLSelectElement>document.getElementById('awardName')).value;
+  //   // console.log(nameSelected);
+  //   if (nameSelected !== 'other') {
+  //     this.showOtherNameInput = false;
+  //   } else {
+  //     this.showOtherNameInput = true;
+  //   }
+  // }
 }
