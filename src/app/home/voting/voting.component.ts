e6fd6c04ad8a27f52284db69c23e997b6b66629a
grayGit <gray@enclave.vn>
2019-05-07 08:03:17 +0700

    Updated graphic UI

--- src/app/home/voting/voting.component.ts
+++ src/app/home/voting/voting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-voting',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./voting.component.scss']
 })
 export class VotingComponent implements OnInit {
-
-  constructor() { }
+  voting: FormGroup;
+  selected: any[] = [];
+  listNominees = [
+    {id: 1, name: 'Chuc (Gray) D. TRUONG'},
+    {id: 2, name: 'Canh (Adam) V. TRUONG'},
+    {id: 3, name: 'Hau (Hammer) T. PHAM'},
+    {id: 4, name: 'Bao (Berny) C. BAO'},
+    {id: 5, name: 'Dinh (Roger) D. LE'}
+  ]
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.generateForm();
+  }
+  generateForm() {
+    this.voting = this.formBuilder.group({
+      id: 'Award Name',
+      first_vote: 'Nominee Name',
+      second_vote: 'Nominee Name',
+      third_vote: 'Nominee Name'
+    });
+  }
+  // after an option selected in select box
+  onChange(event) {
+    const id = event.target.value;
+    const selected = this.listNominees.find(nominee => nominee.id === id);
+    this.selected.push(selected);
+
+  }
+  onFocus(event) {
+
   }
 
 }
