cf965ae4d58b598bf85856b34e0a16ec46146514
grayGit <gray@enclave.vn>
2019-05-09 17:14:43 +0700

    Changed import command at adding space between character

--- src/app/home/voting/voting.component.ts
+++ src/app/home/voting/voting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AwardService} from '../../_services/award.service';
 import {AccountService} from '../../_services/account.service';
