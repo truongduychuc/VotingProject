ffcd425bd1fb4f918f5be2d8e24bb662c6de08f1
grayGit <gray@enclave.vn>
2019-05-14 12:28:42 +0700

    Added routelink at 'View Result'

--- src/app/home/award-management/award/award.component.ts
+++ src/app/home/award-management/award/award.component.ts
@@ -28,7 +28,7 @@ export class AwardComponent implements OnInit {
   // sharedData: for transferring successfully uploading logo message to award-management component
   constructor(private modalService: NgbModal, private sharedData: DataSharingService) {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    console.log(this.currentUser);
+    // console.log(this.currentUser);
   }
   ngOnInit() {
   }
