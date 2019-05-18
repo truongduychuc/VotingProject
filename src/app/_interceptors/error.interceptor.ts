5ab21952e348600c85092e8653207a45937d9441
grayGit <gray@enclave.vn>
2019-05-18 20:36:05 +0700

    Deleted all alert cmd

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -11,6 +11,7 @@ import {Observable, throwError} from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
 import {Router} from '@angular/router';
 
+
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
   constructor(private router: Router) {
@@ -18,24 +19,29 @@ export class ErrorInterceptor implements HttpInterceptor{
  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(tap(evt => {
       if (evt instanceof HttpResponse) {
-        if (evt.body && evt.body.success) {
-          console.log(evt.body.success.message);
-          // alert(evt.body.success.message);
+        // if response has no error and takes along message
+        if (evt.body && evt.body.message) {
+          console.log(evt.body.message);
+          // this.notifier.notify('info', evt.body.message);
         }
       }
     }), catchError((err: HttpErrorResponse) => {
       if (err.status === 401) {
         console.log(err.error.message);
+        // this.notifier.notify('error', err.error.message);
         this.router.navigate(['default']);
       }
       if (err.status === 400) {
+        // this.notifier.notify('error', err.error.message);
         console.log(err.error.message);
       }
       if (err.status === 404) {
+        // this.notifier.notify('error', err.error.message);
         console.log(err.error.message);
         // alert(err.error.message); //alert, for example: 'User or password is incorrect'
       }
       if (err.status === 403) {
+        // this.notifier.notify('error', err.error.message);
         console.log(err.error.message);
         // alert(err.error.message);
       }
