057e06ae4810921477401c29effc141ec5c998be
grayGit <gray@enclave.vn>
2019-05-19 15:01:53 +0700

    Updated confirm modal

--- src/app/modals/confirm-modal/confirm-modal.component.ts
+++ src/app/modals/confirm-modal/confirm-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-confirm-modal',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm-modal.component.scss']
 })
 export class ConfirmModalComponent implements OnInit {
-
-  constructor() { }
+  @Input()
+  title: string;
+  @Input()
+  content: string;
+  @Input()
+  type: string = 'danger'; // default type
+  constructor(private activeModal: NgbActiveModal) { }
 
   ngOnInit() {
   }
-
+  get themeColor() {
+    if (this.type === 'danger') {
+      return 'modal-confirm-danger';
+    }
+    if (this.type === 'primary') {
+      return  'modal-confirm-info';
+    }
+  }
+  get buttonColor() {
+    return this.type;
+  }
 }
