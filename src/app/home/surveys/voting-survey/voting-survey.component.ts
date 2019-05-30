f0cedd6f0a638373fff6f836e35bc0499f40e20d
grayGit <gray@enclave.vn>
2019-05-30 12:23:34 +0700

    Updated survey component

--- src/app/home/surveys/voting-survey/voting-survey.component.ts
+++ src/app/home/surveys/voting-survey/voting-survey.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SurveyService} from '../../../_services/survey.service';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NotifierService} from 'angular-notifier';
 
 @Component({
   selector: 'app-voting-survey',
@@ -10,7 +12,8 @@ export class VotingSurveyComponent implements OnInit {
   surveyList: any[];
   questionList: any[];
   selectedSurveyId: number;
-  constructor(private surveyService: SurveyService) { }
+  votingSurvey: FormGroup;
+  constructor(private surveyService: SurveyService, private formBuilder: FormBuilder, private notifier: NotifierService) { }
 
   ngOnInit() {
     this.surveyService.getListSurveys().subscribe( list => {
@@ -18,14 +21,44 @@ export class VotingSurveyComponent implements OnInit {
     }, err => {
       console.log(err);
     });
+    this.generateForm();
+  }
+  generateForm() {
+    this.votingSurvey = this.formBuilder.group({
+      survey_id: [this.selectedSurveyId],
+      answers: this.formBuilder.array([this.createAnswer()])
+    });
+  }
+  createAnswer() {
+    return this.formBuilder.group({
+      id: ['', Validators.required]
+    });
   }
   getQuestionList(survey_id: number) {
     this.surveyService.getQuestionList(survey_id).subscribe( questionList => {
       this.questionList = questionList;
-      console.log(this.questionList);
+      // console.log(this.questionList);
+      const answers = this.votingSurvey.get('answers') as FormArray;
+      this.questionList.forEach( (question, index) => {
+        if (index < this.questionList.length - 1) {
+          answers.push(this.createAnswer());
+        } else {
+          return;
+        }
+      });
     }, err => {
       console.log(err);
     });
   }
-
+  onSubmit() {
+    console.log(this.votingSurvey.value);
+    if (this.votingSurvey.invalid) {
+      return;
+    }
+    this.surveyService.voteForSurvey(this.votingSurvey.value).subscribe( success => {
+      this.notifier.notify('info', 'Voted successfully!');
+    }, err => {
+      console.log(err);
+    });
+  }
 }
